fdd04a0c25c983c81fa495f4d91da8a7cec63b3b
Graeme Lockley <graemel@no9.co.za>
2020-09-30 06:48:21 +0200

    refactor: change from function to arrow notation

--- mod.ts
+++ mod.ts
@@ -5,7 +5,7 @@ export const emptySet = new Set();
  * 
  * @param v either a value or collection of values
  */
-export function setOf<T>(v: T | Array<T>): Set<T> {
+export const setOf = <T>(v: T | Array<T>): Set<T> => {
   const result = new Set<T>();
 
   if (v instanceof Array) {
@@ -16,12 +16,12 @@ export function setOf<T>(v: T | Array<T>): Set<T> {
     result.add(v);
   }
   return result;
-}
+};
 
 /**
  * Constructs a set of numbers composed of the values in the range `from`..`to`.  Note if `from` is greater than `to` then an empty set is returned.
  */
-export function rangeSet(from: number, to: number): Set<number> {
+export const rangeSet = (from: number, to: number): Set<number> => {
   if (from < to) {
     const result = new Set<number>();
 
@@ -36,33 +36,27 @@ export function rangeSet(from: number, to: number): Set<number> {
   } else {
     return emptySet as Set<number>;
   }
-}
+};
 
 /**
  * Returns whether or `set` has any elements.
  */
-export function isEmpty<T>(set: Set<T>): boolean {
-  return set.size == 0;
-}
+export const isEmpty = <T>(set: Set<T>): boolean => set.size == 0;
 
 /**
  * Returns whether or not `set` contains a single element
  */
-export function isSingleton<T>(set: Set<T>): boolean {
-  return set.size == 1;
-}
+export const isSingleton = <T>(set: Set<T>): boolean => set.size == 1;
 
 /**
  * Returns an element from `set`.
  */
-export function first<T>(set: Set<T>): T {
-  return [...set][0];
-}
+export const first = <T>(set: Set<T>): T => [...set][0];
 
 /**
  * Returns a new set composed of the intersection of `a` and `b`.
  */
-export function intersection<T>(a: Set<T>, b: Set<T>): Set<T> {
+export const intersection = <T>(a: Set<T>, b: Set<T>): Set<T> => {
   const result = new Set<T>();
 
   a.forEach((element) => {
@@ -72,12 +66,12 @@ export function intersection<T>(a: Set<T>, b: Set<T>): Set<T> {
   });
 
   return result;
-}
+};
 
 /**
  * Returns a new set composed of all elements in `source` that are no in `extract`.
  */
-export function minus<T>(source: Set<T>, extract: Set<T>): Set<T> {
+export const minus = <T>(source: Set<T>, extract: Set<T>): Set<T> => {
   if (isEmpty(intersection(source, extract))) {
     return source;
   } else {
@@ -91,12 +85,12 @@ export function minus<T>(source: Set<T>, extract: Set<T>): Set<T> {
 
     return result;
   }
-}
+};
 
 /**
  * Returns whether or not all elements in `a` are also in `b`.
  */
-export function isSubsetOf<T>(a: Set<T>, b: Set<T>): boolean {
+export const isSubsetOf = <T>(a: Set<T>, b: Set<T>): boolean => {
   for (const element of a) {
     if (!b.has(element)) {
       return false;
@@ -104,7 +98,7 @@ export function isSubsetOf<T>(a: Set<T>, b: Set<T>): boolean {
   }
 
   return true;
-}
+};
 
 /**
  * Returns whether or not `a` and `b` are the same.  In this library equality is defined as
@@ -113,14 +107,13 @@ export function isSubsetOf<T>(a: Set<T>, b: Set<T>): boolean {
  * isEqual(a, b) == isSubsetOf(a, b) && isSubsetOf(b, a)
  * ```
  */
-export function isEqual<T>(a: Set<T>, b: Set<T>): boolean {
-  return a.size == b.size && isSubsetOf(a, b) && isSubsetOf(b, a);
-}
+export const isEqual = <T>(a: Set<T>, b: Set<T>): boolean =>
+  a.size == b.size && isSubsetOf(a, b) && isSubsetOf(b, a);
 
 /**
  * Returns a set composed of all the elements in `a` combined with all the elments in `b`.
  */
-export function union<T>(a: Set<T>, b: Set<T>): Set<T> {
+export const union = <T>(a: Set<T>, b: Set<T>): Set<T> => {
   if (isSubsetOf(a, b)) {
     return b;
   } else if (isSubsetOf(b, a)) {
@@ -134,14 +127,12 @@ export function union<T>(a: Set<T>, b: Set<T>): Set<T> {
 
     return result;
   }
-}
+};
 
 /**
  * Returns an array composed of all the elements in `s`.  No assumption should be made on the order of the elements on the result.
  */
-export function asArray<T>(s: Set<T>): Array<T> {
-  return [...s];
-}
+export const asArray = <T>(s: Set<T>): Array<T> => [...s];
 
 /**
  * This type is used to represent the contents of a set.  So the set {1, 2, 3, 6, 7, 10, 11, 12} can be represented as
@@ -161,7 +152,7 @@ export type SetRange = number | [number, number];
 /**
  * Returns `s` as an array of [SetRange] elements.
  */
-export function asRanges(s: Set<number>): Array<SetRange> {
+export const asRanges = (s: Set<number>): Array<SetRange> => {
   const result: Array<SetRange> = [];
 
   const setAsSortedList = asArray(s).sort((a, b) => a - b);
@@ -191,7 +182,7 @@ export function asRanges(s: Set<number>): Array<SetRange> {
   }
 
   return result;
-}
+};
 
 /**
  * Returns a filtered set containing only those elements in `s` where the predicate `p` is satisfied.
